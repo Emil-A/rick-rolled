c4ad25d1b7968cafc760a2250d343efb7bff4729
Emil Aite <aite.emil@gmail.com>
2016-06-18 19:34:58 -0400

    Have everything need from trends

--- pull_info.js
+++ pull_info.js
@@ -4,7 +4,21 @@ var googleTrends = require('google-trends-api');
 
 googleTrends.hotTrendsDetail()
 .then(function(results){
-	console.log(results.rss.channel[0].item[0]);
+	var desc = "";
+	var snippets = results.rss.channel[0].item[0]['ht:news_item'];
+	for (var i = 0, len = snippets.length; i < len; i++) {
+		//console.log(snippets);
+		desc += snippets[i]['ht:news_item_snippet'];
+	}
+
+	var obj = results.rss.channel[0].item[0];
+	var title = obj.title;
+	var pic = "https:" + obj['ht:picture'];
+
+	console.log("title: " + title);
+	console.log("pic: " + pic);
+	console.log("desc: " + desc);
+
 })
 .catch(function(err){
 	console.log("there was an err", err);
